23076513e77997acf8c03dc6311da492518ba10b
BryanCachimuel <bryanloyo56@gmail.com>
2022-10-20 22:09:46 -0500

    estructurando el proyecto y ejecutando el servidor

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,9 @@
+import app from './app'
+import { PORT } from './config'
 
+async function main(){
+    app.listen(PORT)
+    console.log('Servidor en el puerto http://localhost:3002');
+}
+
+main()
