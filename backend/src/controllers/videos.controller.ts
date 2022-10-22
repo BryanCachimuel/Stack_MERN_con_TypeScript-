a428c54f1c9e990bce0c3932f951b7528ede604b
BryanCachimuel <bryanloyo56@gmail.com>
2022-10-21 20:48:19 -0500

    creando el método para crear un nuevo registro de un video

--- backend/src/controllers/videos.controller.ts
+++ backend/src/controllers/videos.controller.ts
@@ -1,21 +1,34 @@
-import { RequestHandler } from 'express'
+import { RequestHandler, Request, Response } from 'express'
+import Video from '../models/video.model'
 
-export const obteniendoVideos: RequestHandler = (req, res) => {
+export const obteniendoVideos: RequestHandler = (req: Request, res: Response) => {
     res.json('Obteniendo videos desde controller')
 }   
 
-export const crearVideo: RequestHandler = (req, res) => {
-    res.json('Crear Proyecto')
+export const crearVideo: RequestHandler = async (req: Request, res: Response) => {
+    try {
+        const videoEncontrado = await Video.findOne({url: req.body.url})
+        if(videoEncontrado){
+            return res.status(301).json({ message: "La URL de este video ya existe" })
+        }
+
+        const videoNuevo = new Video(req.body)
+        const crearVideo = await videoNuevo.save()
+        res.json(crearVideo)
+             
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-export const obteniendoVideoPorId: RequestHandler = (req, res) => {
+export const obteniendoVideoPorId: RequestHandler = (req: Request, res: Response) => {
     res.json('Obteniendo video')
 }  
 
-export const editarVideo: RequestHandler = (req, res) => {
+export const editarVideo: RequestHandler = (req: Request, res: Response) => {
     res.json('Editar video')
 } 
 
-export const eliminarVideos: RequestHandler = (req, res) => {
+export const eliminarVideos: RequestHandler = (req: Request, res: Response) => {
     res.json('Eliminar video')
 }  
