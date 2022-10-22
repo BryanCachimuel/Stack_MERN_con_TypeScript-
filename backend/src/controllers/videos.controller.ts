88999e80ff0984a6de76b2a471a902b1491aa8e7
BryanCachimuel <bryanloyo56@gmail.com>
2022-10-21 21:13:01 -0500

    agregando los métodos para lista todos los videos y obtener un video mediante su id

--- backend/src/controllers/videos.controller.ts
+++ backend/src/controllers/videos.controller.ts
@@ -1,8 +1,13 @@
 import { RequestHandler, Request, Response } from 'express'
 import Video from '../models/video.model'
 
-export const obteniendoVideos: RequestHandler = (req: Request, res: Response) => {
-    res.json('Obteniendo videos desde controller')
+export const obteniendoVideos: RequestHandler = async (req: Request, res: Response) => {
+    try {
+        const videos = await Video.find()
+        return res.json(videos)
+    } catch (error) {
+        res.json(error)
+    }   
 }   
 
 export const crearVideo: RequestHandler = async (req: Request, res: Response) => {
@@ -17,12 +22,20 @@ export const crearVideo: RequestHandler = async (req: Request, res: Response) =>
         res.json(crearVideo)
              
     } catch (error) {
-        console.log(error)
+        res.json(error)
     }
 }
 
-export const obteniendoVideoPorId: RequestHandler = (req: Request, res: Response) => {
-    res.json('Obteniendo video')
+export const obteniendoVideoPorId: RequestHandler = async (req: Request, res: Response) => {
+    try {
+        const videoporId = await Video.findById(req.params.id)
+        if(!videoporId){
+            res.status(204).json()
+        }
+        return res.json(videoporId)
+    } catch (error) {
+        res.json(error)
+    }
 }  
 
 export const editarVideo: RequestHandler = (req: Request, res: Response) => {
