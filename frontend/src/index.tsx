57ee491de0c791b7e7145494c82d05bd5ba72680
BryanCachimuel <bryanloyo56@gmail.com>
2022-10-23 20:00:56 -0500

    agregando estilos a react con bootswatch

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ListaVideos from './components/Videos/ListaVideos';
 import CrearVideo from './components/Videos/CrearVideo';
+import 'bootswatch/dist/zephyr/bootstrap.min.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
